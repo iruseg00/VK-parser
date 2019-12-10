64eebdacfc32e54ab5fca8aa875cc61b551244ce
iruseg00 <isiruseg@gmail.com>
2019-12-10 19:11:15 +0300

    migration failed

--- parser/db/migrations/20191206105400-posts.js
+++ parser/db/migrations/20191206105400-posts.js
@@ -2,73 +2,73 @@
 
 module.exports = 
 {
-    up: (queryInterface, Sequelize) => 
+  up: (queryInterface, Sequelize) => 
+  {
+    return queryInterface.createTable("Posts",
     {
-        return queryInterface.createTable("Posts",
+      id: 
+      {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+      type: 
+      {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      topic: 
+      {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      description: 
+      {
+        type: Sequelize.TEXT,
+        get: function() 
         {
-            id: 
-            {
-              type: Sequelize.UUID,
-              primaryKey: true,
-              defaultValue: Sequelize.UUIDV4
-            },
-            type: 
-            {
-              type: Sequelize.STRING,
-              allowNull: true
-            },
-            topic: 
-            {
-              type: Sequelize.TEXT,
-              allowNull: true
-            },
-            description: 
-            {
-              type: Sequelize.TEXT,
-              get: function() 
-              {
-                return JSON.parse(this.getDataValue('description'));
-              }, 
-              set: function(val) 
-              {
-                return this.setDataValue('description', JSON.stringify(val));
-              },
-              allowNull: false
-            },
-            linksPhoto: 
-            {
-              type: Sequelize.TEXT,
-              get: function() 
-              {
-                return JSON.parse(this.getDataValue('linksPhoto'));
-              }, 
-              set: function(val) 
-              {
-                return this.setDataValue('linksPhoto', JSON.stringify(val));
-              },
-              allowNull: true
-            },
-            link: 
-            {
-              type: Sequelize.STRING,
-              allowNull: false
-            },
-            timeUTC: 
-            {
-              type: Sequelize.DATE,
-              allowNull: true
-            },
-            site: 
-            {
-              type: Sequelize.STRING,
-              allowNull: false
-            },  
-            createdAt: Sequelize.DATE,
-            updatedAt: Sequelize.DATE,
-        });
-    },
-    down: (queryInterface, Sequelize) => 
-    {
-      return queryInterface.dropTable("Posts");
-    }
+          return JSON.parse(this.getDataValue('description'));
+        }, 
+        set: function(val) 
+        {
+          return this.setDataValue('description', JSON.stringify(val));
+        },
+        allowNull: false
+      },
+      linksPhoto: 
+      {
+        type: Sequelize.TEXT,
+        get: function() 
+        {
+          return JSON.parse(this.getDataValue('linksPhoto'));
+        }, 
+        set: function(val) 
+        {
+          return this.setDataValue('linksPhoto', JSON.stringify(val));
+        },
+        allowNull: true
+      },
+      link: 
+      {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      timeUTC: 
+      {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      site: 
+      {
+        type: Sequelize.STRING,
+        allowNull: false
+      },  
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+  down: (queryInterface, Sequelize) => 
+  {
+    return queryInterface.dropTable("Posts");
+  }
 };
