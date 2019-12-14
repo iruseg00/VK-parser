e844480ea7e0ae427be34b1b8914ca30bca48d66
iruseg00 <isiruseg@gmail.com>
2019-12-14 20:30:03 +0300

    puppeteer  FINISH NAH docker

--- parser/sites/epic_games/EpicGames.js
+++ parser/sites/epic_games/EpicGames.js
@@ -12,65 +12,54 @@ function sleep(ms)
 
 function EpicGames()
 {
-    try 
-    {
-        logger.info('start eg parsing');
-        (async () => {
-            try 
-            {//{ executablePath: 'google-chrome-unstable' }
-                const browser = await puppeteer.launch({
-                    args: ['--disable-dev-shm-usage' , '--no-sandbox']
-                  });
-                const page = await browser.newPage();
-                await page.goto(link);
-                await page.waitForSelector("div.Discover-section_c504570a");
-                await sleep(20000);
-                const html = await page.content();    
-                await browser.close(); 
-                logger.info('browser closed');
-                var $ = cheerio.load(html);
-                $('a').each( (i , elem)=>
+    (async () => {
+        try 
+        {
+            const browser = await puppeteer.launch({
+                args: ['--disable-dev-shm-usage' , '--no-sandbox']
+              });
+            const page = await browser.newPage();
+            await page.goto(link);
+            await page.waitForSelector("div.Discover-section_c504570a");
+            await sleep(20000);
+            const html = await page.content();    
+            await browser.close(); 
+            var $ = cheerio.load(html);
+            $('a').each( (i , elem)=>
+            {
+                var a = String($(elem).attr('aria-label'));
+                if ( a.match(/Free Game Every Week/i) )
                 {
-                    var a = String($(elem).attr('aria-label'));
-                    if ( a.match(/Free Game Every Week/i) )
+                    var object = {linksPhoto: []};
+                    object.site = "www.epicgames.com";
+                    object.type = "Раздача игр";
+                    object.topic = a.substring(29 , a.indexOf(',' , 30 ));
+                    object.link = ("https://www.epicgames.com" + $(elem).attr('href'));
+                    object.timeUTC = ($('span > span > time' , elem).attr('datetime'));
+                    var DIV_Card_content = String($( 'div' , elem).attr('class'));
+                    if(DIV_Card_content.match(/Card-content_/i))
                     {
-                        logger.info('Extract a = ' + a);
-                        var object = {linksPhoto: []};
-                        object.site = "www.epicgames.com";
-                        object.type = "Раздача игр";
-                        object.topic = a.substring(29 , a.indexOf(',' , 30 ));
-                        object.link = ("https://www.epicgames.com" + $(elem).attr('href'));
-                        object.timeUTC = ($('span > span > time' , elem).attr('datetime'));
-                        var DIV_Card_content = String($( 'div' , elem).attr('class'));
-                        if(DIV_Card_content.match(/Card-content_/i))
+                        var DIV_Card_imageW = String($('div > div' , elem).attr('class'));
+                        if(DIV_Card_imageW.match(/Card-imageWrapper/i))
                         {
-                            var DIV_Card_imageW = String($('div > div' , elem).attr('class'));
-                            if(DIV_Card_imageW.match(/Card-imageWrapper/i))
-                            {
-                                var DIV_Card_image = String($(`div.${DIV_Card_imageW} > div` , `div.${DIV_Card_content}` , elem).attr('class'));
-                                if(DIV_Card_image.match(/Card-image_/i))
-                                {    
-                                    object.linksPhoto.push(String(($('img' , elem).attr('data-image'))));
-                                    object.text = ["FREE"];
-                                    logger.info('OBJECT EPICGAMES: ' + object);
-                                    PostsService.create(object);
-                                }
+                            var DIV_Card_image = String($(`div.${DIV_Card_imageW} > div` , `div.${DIV_Card_content}` , elem).attr('class'));
+                            if(DIV_Card_image.match(/Card-image_/i))
+                            {    
+                                object.linksPhoto.push(String(($('img' , elem).attr('data-image'))));
+                                object.text = ["FREE"];
+                                PostsService.create(object);
                             }
                         }
-                        delete object;
                     }
-                }); 
-            }
-            catch(error)
-            {
-                logger.error('error in EpicGames.js -> browser , error: ' + error);
-            }       
-          })();
-    } 
-    catch (error) 
-    {
-        logger.error('error in EpicGames.js , error: ' + error);
-    }
+                    delete object;
+                }
+            }); 
+        }
+        catch(error)
+        {
+            logger.error('error in EpicGames.js -> browser , error: ' + error);
+        }       
+    })();
 }
 
 
