68e72553b2704036735766031df80af502399024
Tumkin <cm.tumkin@yandex.ru>
2020-03-03 19:46:14 +0300

    Update CrackwatchController.js

--- parser/controllers/CrackwatchController.js
+++ parser/controllers/CrackwatchController.js
@@ -6,7 +6,7 @@ const CrackwatchService = require('../services/CrackwatchService');
 router.post("/add", (req, res) =>     
 {
   logger.info('POST request success');
-  CrackwatchService.AddTracking(req.body.link)
+  CrackwatchService.AddTracking(req)
     .then(data => 
     {
       if (!data) throw new Error("Error in CrackwatchController.js , can not get data by method CrackwatchService.AddTracking()");
