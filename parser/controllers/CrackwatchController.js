6313699704c8ec3c4a61b98db43caa536e9ac01c
iruseg00 <isiruseg@gmail.com>
2020-03-03 19:27:03 +0300

    log info in post req

--- parser/controllers/CrackwatchController.js
+++ parser/controllers/CrackwatchController.js
@@ -5,10 +5,12 @@ const CrackwatchService = require('../services/CrackwatchService');
 
 router.post("/add", (req, res) =>     
 {
+  logger.info('POST request success');
   CrackwatchService.AddTracking(req.body.link)
     .then(data => 
     {
       if (!data) throw new Error("Error in CrackwatchController.js , can not get data by method CrackwatchService.AddTracking()");
+      logger.info('Add function start');
       res.status(200);
     })
     .catch(err => {
